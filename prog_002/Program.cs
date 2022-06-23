27a51de43549ec50f6ac248d7fd0f804f22b41b2
Mikhaylov Artem <gits.info20@gmail.com>
2022-06-23 23:35:55 +0300

    seminar 6

---
+++ prog_002/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 42:** Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+*/
+
+using static System.Console;
+Clear();
+WriteLine("Введите число.");
+int nuber10=int.Parse(ReadLine());
+int size=$"{nuber10}".Length;
+WriteLine(BinNumber(nuber10));
+
+
+string BinNumber(int number)
+{
+    string number_bin="";
+    while (number>0)
+    {
+    
+        if(number%2==0)
+        {
+            number_bin+="1";
+         }
+         else
+         {
+            number_bin+="0";
+         }
+         number/=2;
+    }
+    return number_bin;
+}
---
+++ prog_003/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 44:** Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа 
+Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+using static System.Console;
+Clear();
+
+
+WriteLine("Введите число.");
+int a=int.Parse(ReadLine());
+FibArr(a);
+
+
+
+
+void FibArr(int size)
+{
+    if (size==0) WriteLine($"[{size}]");
+    else if (size==1) WriteLine($"[{(size-1)}, {size}]");
+    else
+        {
+            int [] result=new int [size];
+            result[0]=0;
+            result[1]=1;
+            for(int i=2; i<size;i++)
+            {
+                result[i]=result[i-1]+result[i-2];
+            }
+            WriteLine($"[{String.Join(" ,", result)}]");
+        }
+}
